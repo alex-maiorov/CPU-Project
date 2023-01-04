1dad0d8dc9baf184cfc0e639b0bb3aa88d6c0ad3
alex <alexander.mmaiorov@gmail.com>
2023-01-04 13:18:28 -0600

    made division

--- src/div.v
+++ src/div.v
@@ -6,26 +6,51 @@ input clk,
 input rst,
 input signctl, //pull high to treat as twos complement signed, pull low to treat as unsigned
 input remainder_out, //pull low to get quotient, pull high to get remainder
+output reg [31:0] dout,
+output drdy
 )
 
 reg [31:0] Q_intermediate;
 reg [31:0] R_intermediate;
 
-assign Q = Q_intermediate;
-assign R = R_intermediate;
-
-assign drdy = (R_intermediate > B)
+always @(*) begin
+    case (remainder_out)
+        1'b0: assign dout = Q_intermediate;
+        1'b1: assign dout = R_intermediate;
+    endcase 
+    
+    case ({signctl, B[31]})
+        2'b00: assign drdy = (B < R_intermediate);
+        2'b01: assign drdy = (B < R_intermediate);
+        2'b10: assign drdy = (B < R_intermediate);
+        2'b11: assign drdy = (B > R_intermediate);
+    endcase
+end
 
 always @(posedge clk) begin
-    case {(R_intermediate < B), B[31]
-        1'b1: begin
-            R_intermediate = R_intermediate - B;
+    case ({drdy, signctl, (B[31] ^ A[31])}) //god I hope this will work
+        3'b000: begin //unsigned
             Q_intermediate = Q_intermediate + 1;
+            R_intermediate = R_intermediate - B;
         end
+        3'b001: begin //unsigned
+            Q_intermediate = Q_intermediate + 1;
+            R_intermediate = R_intermediate - B;
+        end
+        3'b010: begin //signed, A and B are both of the same sign
+            Q_intermediate = Q_intermediate + 1;
+            R_intermediate = R_intermediate - B;
+        end
+        3'b011: begin //signed, A and B are of different signs
+            Q_intermediate = Q_intermediate - 1;
+            R_intermediate = R_intermediate + B;
+        default: ;
     endcase
 end
 
 always @(posedge rst) begin
     R_intermediate = A;
     Q_intermediate = 32'h00000000;
+end
+
 endmodule;
---
+++ verification/tb/div_tb.v
@@ -0,0 +1,77 @@
+`include "div.v"
+`timescale 10us/100ns
+module mul_tb();
+
+reg [31:0] a;
+reg [31:0] b;
+reg clk;
+reg rst;
+reg signctl;
+reg remainder_out; 
+wire [31:0] dout;
+wire drdy;
+reg [17:0] i;
+integer index;
+
+div div_test(a,
+b,
+clk,
+rst,
+signctl,
+remainder_out,
+dout,
+drdy);
+
+
+
+initial begin
+	$display ("BEGIN CSV div");
+	clk = 1'b0;
+	rst = 1'b1;
+	#5;
+	rst = 0'b0; //reset multiplication circuit before starting
+	i = {19{1'b0}};
+	$display ("time, clk, rst, signctl, remainder_out, a, b, dout, drdy");
+	index = 0;
+	while(index<2**(2+8+8)) begin //test 4 MSBs and 4 LSBs
+
+		a = {i[7:4], {24{1'b0}}, i[3:0]};
+		b = {i[15:12], {24{1'b0}}, i[11:8]};
+
+		signctl = i[16];
+		remainder_out = i[17];
+		// a = 32'h00000006;
+		// b = 32'h00000006;
+		// signctl = 1'b0;
+		// upper = 1'b0;
+		
+		#5;
+		// $display ("%g,%h,%h,%h,%h,%h,%h,%h,%h", 
+		// $time, clk, rst, signctl, upper, a, b, dout, drdy);
+		if(drdy == 1'b1) begin
+
+			
+			$display ("%g,%h,%h,%h,%h,%h,%h,%h,%h", 
+			$time, clk, rst, signctl, remainder_out, a, b, dout, drdy);
+			index = index + 1;
+			i = i + 1'b1;
+			rst = 1'b1;
+			#5;
+
+			rst = 1'b0;
+			// $finish;
+		end
+	end
+	$display ("END CSV div");
+	$finish;
+end
+
+always begin
+	#5 clk = ~clk;
+end
+//always @(*) begin
+//	$strobe ("%g,%h,%h,%h,%h,%h", 
+//		$time, control, a, b, dout, cout);
+//end
+
+endmodule
